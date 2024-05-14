df953694289e6eb562ec31f10fe666a8e4483b34
henrique0927 <henrique_stolle@estudante.sesisenai.org.br>
2024-05-14 07:53:13 -0300

    Lógica JS do Exercicio dois concluido

--- ex-2.js
+++ ex-2.js
@@ -1,25 +1,24 @@
 function calcular() {
-    const nome = document.getElementById('nome').value;
-    const salario = parseFloat(document.getElementById('salario')).value;
-    const inss = document.getElementById('inss').value;
-    const liquido = document.getElementById('liquido').value;
+    const salario = parseFloat(document.getElementById('salario').value);
+    const inssInput = document.getElementById('inss');
+    const liquidoInput = document.getElementById('liquido');
+
+    let inss = 0;
+    let liquido = 0;
 
     if (salario <= 1412) {
-       inss = salario * (75/100);
-       liquido = (salario - inss);
-       inss.innerHTML = salario;
+        inss = salario * 0.075;
     } else if (salario > 1412 && salario <= 2666.68) {
-        inss = salario * (90/100);
-        liquido = (salario - inss);
-        inss.innerHTML = salario;
+        inss = salario * 0.09; 
     } else if (salario > 2666.68 && salario <= 4000) {
-        inss = salario * (120/100);
-        liquido = (salario - inss);
-        inss.innerHTML = salario;
+        inss = salario * 0.12;
     } else {
-        inss = salario * (140/100);
-        liquido = (salario - inss);
-        inss.innerHTML = inss;
-        liquido.innerHTML = liquido;
+        inss = salario * 0.14;  
     }
+
+    liquido = salario - inss;
+
+    inssInput.value = inss.toFixed(2);
+    liquidoInput.value = liquido.toFixed(2);
 }
+
